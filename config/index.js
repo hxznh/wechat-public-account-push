2acdc70d16fd2a9e17dba415175dc8d86e1ccc66
hxznh <93560933+hxznh@users.noreply.github.com>
2022-09-24 00:41:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -138,7 +138,7 @@ export const config = {
     * */
   SLOT_LIST: [
     // 这样配置的话，就会每次发送这句话
-    {"keyword": "daily", contents: "该收拾行李啦，要准备去学校辽。"},
+    {"keyword": "daily", contents: "坐飞机去学校咯。"},
     // 这样配置的话，就会每次随机选一句话发送
 //     {"keyword": "lover_prattle", contents: [
 //       "因为太喜欢你，所以看谁都像是情敌。",
