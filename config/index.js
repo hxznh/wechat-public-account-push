818821ddc12894edfdf2b109de419d74347ca7e7
hxznh <93560933+hxznh@users.noreply.github.com>
2022-12-30 23:57:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -138,7 +138,7 @@ export const config = {
     * */
   SLOT_LIST: [
     // 这样配置的话，就会每次发送这句话
-    {"keyword": "daily", contents: "还是要好好努力，想赚钱钱。"},
+    {"keyword": "daily", contents: "好困，睡不够。"},
     // 这样配置的话，就会每次随机选一句话发送
 //     {"keyword": "lover_prattle", contents: [
 //       "因为太喜欢你，所以看谁都像是情敌。",
